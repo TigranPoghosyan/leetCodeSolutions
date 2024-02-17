8cc83f619c16547b71b56034edff74960cb0373a
Tigran Poghosyan <yerevan7971@gmail.com>
2024-02-17 14:28:49 +0400

    21. Merge Two Sorted Lists

---
+++ src/main/java/leetCodeSolutions/mergeTwoSortedLists/Main.java
@@ -0,0 +1,12 @@
+package leetCodeSolutions.mergeTwoSortedLists;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        ListNode list1 = new ListNode(1, new ListNode(3, new ListNode(5)));
+        ListNode list2 = new ListNode(2, new ListNode(4, new ListNode(6)));
+        ListNode merged = solution.mergeTwoLists(list1, list2);
+        System.out.println(merged);
+    }
+}
