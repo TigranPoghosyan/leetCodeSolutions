2842227467a0e2bbf9c53812678958648501e3e4
Tigran Poghosyan <yerevan7971@gmail.com>
2024-02-17 14:50:10 +0400

    1. Two Sum

---
+++ src/main/java/leetCodeSolutions/twoSum/Main.java
@@ -0,0 +1,12 @@
+package leetCodeSolutions.twoSum;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = new int[]{2, 7, 11, 15};
+        int[] ints = solution.twoSum(nums, 9);
+        System.out.println(Arrays.toString(ints));
+    }
+}
