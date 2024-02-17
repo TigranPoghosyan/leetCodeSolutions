d3eba76c06783dfa7af0eb316f85b6b0c748910e
Tigran Poghosyan <yerevan7971@gmail.com>
2024-02-17 19:26:44 +0400

    19. Remove Nth Node From End of List

---
+++ src/main/java/leetCodeSolutions/mediumProblems/RemoveNthNodeFromEndOfList19/Main.java
@@ -0,0 +1,10 @@
+package leetCodeSolutions.mediumProblems.RemoveNthNodeFromEndOfList19;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode list1 = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4))));
+        ListNode listNode = solution.removeNthFromEnd(list1, 3);
+        System.out.println(listNode);
+    }
+}
---
+++ src/main/java/leetCodeSolutions/mediumProblems/RemoveNthNodeFromEndOfList19/Solution.java
@@ -0,0 +1,20 @@
+package leetCodeSolutions.mediumProblems.RemoveNthNodeFromEndOfList19;
+
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode start = new ListNode(0);
+        ListNode slow = start;
+        ListNode fast = start;
+        start.next = head;
+
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+        while (fast != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return start.next;
+    }
+}
