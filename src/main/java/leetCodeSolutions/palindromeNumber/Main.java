1116dc1eb3e328f7b097c91220e7e0e0811d9ffe
Tigran Poghosyan <yerevan7971@gmail.com>
2024-02-17 15:07:45 +0400

    9. Palindrome Number

--- src/main/java/leetCodeSolutions/palindromeNumber/Main.java
+++ src/main/java/leetCodeSolutions/palindromeNumber/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
         boolean palindrome = solution.isPalindrome(121);
+        boolean anotherTestCase = solution.isPalindrome(122);
         System.out.println(palindrome);
+        System.out.println(anotherTestCase);
     }
 }
