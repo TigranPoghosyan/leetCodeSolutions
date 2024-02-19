2a9953c2d6c06ee15c165d733588ad8a2d7a482a
Tigran Poghosyan <yerevan7971@gmail.com>
2024-02-19 16:12:46 +0400

    167. Two Sum II - Input Array Is Sorted

---
+++ src/main/java/leetCodeSolutions/mediumProblems/TwoSumTwoInputArrayIsSorted167/Solution.java
@@ -0,0 +1,34 @@
+package leetCodeSolutions.mediumProblems.TwoSumTwoInputArrayIsSorted167;
+
+public class Solution {
+    //First solution
+    //Two Pointer Solution
+    public int[] twoSum(int[] numbers, int target) {
+        int start = 0;
+        int end = numbers.length - 1;
+
+        while (start < end) {
+            if (numbers[start] + numbers[end] == target) {
+                return new int[]{start + 1, end + 1};
+            } else if (numbers[start] + numbers[end] < target) {
+                start++;
+            } else if (numbers[start] + numbers[end] > target) {
+                end--;
+            }
+        }
+        return new int[]{start + 1, end + 1};
+    }
+}
+
+
+//Second Solution
+//public int[] twoSum(int[] numbers, int target) {
+//    Map<Integer,Integer> map = new HashMap<>();
+//    for(int i = 0;i<numbers.length-1;i++){
+//        if(map.containsKey(target - numbers[i])){
+//            return new int[]{map.get(target - numbers[i]) + 1,i + 1};
+//        }
+//        map.put(numbers[i],i);
+//    }
+//    return new int[]{};
+//}
