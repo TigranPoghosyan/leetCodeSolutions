62cd54df83617e6825488f11943e406b1e232426
Tigran Poghosyan <yerevan7971@gmail.com>
2024-02-19 21:09:45 +0400

    704. Binary Search

---
+++ src/main/java/leetCodeSolutions/mediumProblems/binarySearch704/Main.java
@@ -0,0 +1,11 @@
+package leetCodeSolutions.mediumProblems.binarySearch704;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = new int[]{-1, 0, 3, 5, 9, 12};
+
+        int result = solution.search(nums, 0);
+        System.out.println(result);
+    }
+}
