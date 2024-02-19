923b98e873adf666899eb11f576f19d2566e2fd6
Tigran Poghosyan <yerevan7971@gmail.com>
2024-02-19 19:59:11 +0400

    7. Reverse Integer

---
+++ src/main/java/leetCodeSolutions/mediumProblems/reverseInteger7/Solution.java
@@ -0,0 +1,19 @@
+package leetCodeSolutions.mediumProblems.reverseInteger7;
+
+public class Solution {
+    public int reverse(int x) {
+        int reverse = 0;
+        int pop;
+
+        while (x != 0) {
+            pop = x % 10;
+            x /= 10;
+            if (reverse > Integer.MAX_VALUE / 10 || reverse == Integer.MAX_VALUE / 10 && pop > 7) return 0;
+            if (reverse < Integer.MIN_VALUE / 10 || reverse == Integer.MIN_VALUE / 10 && pop < -8) return 0;
+
+            reverse = (reverse * 10) + pop;
+        }
+        return reverse;
+
+    }
+}
