8be9c8ed9f662ef3f7b8f4bf1174c6ec4df61304
Tigran Poghosyan <yerevan7971@gmail.com>
2024-02-20 14:59:02 +0400

    876. Middle of the Linked List

---
+++ src/main/java/leetCodeSolutions/easyProblems/middleOfLinkedList876/Solution.java
@@ -0,0 +1,17 @@
+package leetCodeSolutions.easyProblems.middleOfLinkedList876;
+
+public class Solution {
+    public ListNode middleNode(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
