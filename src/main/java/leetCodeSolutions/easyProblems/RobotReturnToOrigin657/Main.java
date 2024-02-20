f1805efca4e0bab9b6926d802aae9f4e86d96cc8
Tigran Poghosyan <yerevan7971@gmail.com>
2024-02-20 15:30:17 +0400

    657. Robot Return to Origin

---
+++ src/main/java/leetCodeSolutions/easyProblems/RobotReturnToOrigin657/Main.java
@@ -0,0 +1,9 @@
+package leetCodeSolutions.easyProblems.RobotReturnToOrigin657;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        boolean result = solution.judgeCircle("UD");
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/leetCodeSolutions/easyProblems/RobotReturnToOrigin657/Solution.java
@@ -0,0 +1,20 @@
+package leetCodeSolutions.easyProblems.RobotReturnToOrigin657;
+
+public class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0;
+        int y = 0;
+        for (char c : moves.toCharArray()) {
+            if (c == 'L') {
+                x -= 1;
+            } else if (c == 'R') {
+                x += 1;
+            } else if (c == 'U') {
+                y += 1;
+            } else if (c == 'D') {
+                y -= 1;
+            }
+        }
+        return x == 0 && y == 0;
+    }
+}
