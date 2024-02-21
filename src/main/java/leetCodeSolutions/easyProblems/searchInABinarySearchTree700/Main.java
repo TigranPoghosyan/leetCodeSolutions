779d7ef198395c7a30550e4bdb685b1269b55d02
Tigran Poghosyan <yerevan7971@gmail.com>
2024-02-21 18:31:59 +0400

    700. Search in a Binary Search Tree

---
+++ src/main/java/leetCodeSolutions/easyProblems/searchInABinarySearchTree700/Main.java
@@ -0,0 +1,14 @@
+package leetCodeSolutions.easyProblems.searchInABinarySearchTree700;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        TreeNode leftOne = new TreeNode(1);
+        TreeNode rightThree = new TreeNode(3);
+        TreeNode rightSeven = new TreeNode(7);
+        TreeNode leftTwo = new TreeNode(2,leftOne,rightThree);
+        TreeNode root = new TreeNode(4,leftTwo,rightSeven);
+        TreeNode result = solution.searchBST(root,2);
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/leetCodeSolutions/easyProblems/searchInABinarySearchTree700/Solution.java
@@ -0,0 +1,18 @@
+package leetCodeSolutions.easyProblems.searchInABinarySearchTree700;
+
+public class Solution {
+    public TreeNode searchBST(TreeNode root,int val){
+        if (root == null){
+            return null;
+        }
+        if(root.val == val){
+            return root;
+        }
+        if(root.val > val){
+            return searchBST(root.left,val);
+        } else if (root.val < val) {
+            return searchBST(root.right,val);
+        }
+        return root;
+    }
+}
