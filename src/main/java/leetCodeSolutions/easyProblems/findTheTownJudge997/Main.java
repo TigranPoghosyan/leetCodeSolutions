b89ff7a3bb8fd6fd1e9322db66578962ddfcb5c0
Tigran Poghosyan <yerevan7971@gmail.com>
2024-02-22 16:55:15 +0400

    997. Find the Town Judge

---
+++ src/main/java/leetCodeSolutions/easyProblems/findTheTownJudge997/Main.java
@@ -0,0 +1,10 @@
+package leetCodeSolutions.easyProblems.findTheTownJudge997;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] nums = {{1, 3}, {2, 3}};
+        int judge = solution.findJudge(3, nums);
+        System.out.println(judge);
+    }
+}
