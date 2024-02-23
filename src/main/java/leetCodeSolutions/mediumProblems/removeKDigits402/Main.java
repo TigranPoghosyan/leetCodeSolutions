da602902ecbd4ee4c8406a7c97512ff077975ab4
Tigran Poghosyan <yerevan7971@gmail.com>
2024-02-23 18:05:44 +0400

    74. Search a 2D Matrix

---
+++ src/main/java/leetCodeSolutions/mediumProblems/removeKDigits402/Main.java
@@ -0,0 +1,10 @@
+package leetCodeSolutions.mediumProblems.removeKDigits402;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String num = "1432219";
+        String result = solution.removeKdigits(num,3);
+        System.out.println(result);
+    }
+}
