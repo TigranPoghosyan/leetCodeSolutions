4c95182bca10c2d6cc2719dc6440cdb1f2c55d68
Tigran Poghosyan <yerevan7971@gmail.com>
2024-02-24 15:31:37 +0400

    482. License Key Formatting

---
+++ src/main/java/leetCodeSolutions/easyProblems/licenseKeyFormatting482/Main.java
@@ -0,0 +1,11 @@
+package leetCodeSolutions.easyProblems.licenseKeyFormatting482;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "5F3Z-2e-9-w";
+        int k = 4;
+        String result = solution.licenseKeyFormatting(s, k);
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/leetCodeSolutions/easyProblems/licenseKeyFormatting482/Solution.java
@@ -0,0 +1,16 @@
+package leetCodeSolutions.easyProblems.licenseKeyFormatting482;
+
+public class Solution {
+    public String licenseKeyFormatting(String s, int k) {
+
+        s = s.toUpperCase();
+        s = s.replaceAll("-","");
+
+        StringBuilder sb = new StringBuilder(s);
+
+        for(int i=s.length()-k;i>0;i=i-k){
+            sb.insert(i,"-");
+        }
+        return sb.toString();
+    }
+}
