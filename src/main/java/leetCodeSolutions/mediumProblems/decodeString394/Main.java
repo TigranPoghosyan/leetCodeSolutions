59c47703569ff9b397737fac942cf7db17655b44
Tigran Poghosyan <yerevan7971@gmail.com>
2024-02-26 20:20:21 +0400

    394. Decode String

---
+++ src/main/java/leetCodeSolutions/mediumProblems/decodeString394/Main.java
@@ -0,0 +1,10 @@
+package leetCodeSolutions.mediumProblems.decodeString394;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "3[a]2[bc]";
+        String result = solution.decodeString(s);
+        System.out.println(result);
+    }
+}
