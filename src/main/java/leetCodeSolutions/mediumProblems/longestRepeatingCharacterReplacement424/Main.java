f3f52339b41bb6143e977422cddad2137b8683a8
Tigran Poghosyan <yerevan7971@gmail.com>
2024-02-27 20:07:20 +0400

    424. Longest Repeating Character Replacement

---
+++ src/main/java/leetCodeSolutions/mediumProblems/longestRepeatingCharacterReplacement424/Main.java
@@ -0,0 +1,9 @@
+package leetCodeSolutions.mediumProblems.longestRepeatingCharacterReplacement424;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int result = solution.characterReplacement("AABABBA", 1);
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/leetCodeSolutions/mediumProblems/longestRepeatingCharacterReplacement424/Solution.java
@@ -0,0 +1,21 @@
+package leetCodeSolutions.mediumProblems.longestRepeatingCharacterReplacement424;
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] arr = new int[26];
+        int largestCount = 0;
+        int start = 0;
+        int maxlen = 0;
+
+        for(int end = 0; end < s.length(); end ++){
+            arr[s.charAt(end) - 'A']++;
+            largestCount = Math.max(largestCount, arr[s.charAt(end) - 'A']);
+            if(end - start + 1 - largestCount > k){
+                arr[s.charAt(start) - 'A']--;
+                start ++;
+            }
+            maxlen = Math.max(maxlen, end - start + 1);
+        }
+        return maxlen;
+    }
+}
