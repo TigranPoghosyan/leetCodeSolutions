8fd4de0a450c4cd81e504943d9985559a78e0785
Tigran Poghosyan <yerevan7971@gmail.com>
2024-02-27 19:38:04 +0400

    27. Remove Element

---
+++ src/main/java/leetCodeSolutions/easyProblems/removeElement27/Main.java
@@ -0,0 +1,10 @@
+package leetCodeSolutions.easyProblems.removeElement27;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {3,2,2,3};
+        int result = solution.removeElement(nums,3);
+        System.out.println(result);
+    }
+}
