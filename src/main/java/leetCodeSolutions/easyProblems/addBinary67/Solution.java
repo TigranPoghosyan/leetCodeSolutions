5c9c7fc8db9e0edfda22104ce9a06aba02b8d289
Tigran Poghosyan <yerevan7971@gmail.com>
2024-02-28 21:35:10 +0400

    67. Add Binary

--- src/main/java/leetCodeSolutions/easyProblems/addBinary67/Solution.java
+++ src/main/java/leetCodeSolutions/easyProblems/addBinary67/Solution.java
@@ -1,21 +1,29 @@
 package leetCodeSolutions.easyProblems.addBinary67;
 
 public class Solution {
-    public String addBinary(String a,String b){
+    public String addBinary(String a, String b) {
+        StringBuilder stringBuilder = new StringBuilder();
+
         int i = a.length() - 1;
         int j = b.length() - 1;
         int carry = 0;
-        StringBuilder result = new StringBuilder();
-        while (i >= 0 || j >= 0){
-            int b1 = (i >= 0) ? Character.getNumericValue(a.charAt(i--)) : 0;
-            int b2 = (j >= 0) ? Character.getNumericValue(b.charAt(j--)) : 0;
-            int sum = b1 + b2 + carry;
-            carry = (sum == 3 || sum == 2) ? 1 : 0;
-            result.append((sum == 3 || sum == 1) ? 1 : 0);
+
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+            }
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+            }
+            stringBuilder.append(sum % 2);
+            carry = sum / 2;
+            i--;
+            j--;
         }
-        if (carry > 0){
-            result.append(1);
+        if (carry > 0) {
+            stringBuilder.append(carry);
         }
-        return result.reverse().toString();
+        return stringBuilder.reverse().toString();
     }
 }
