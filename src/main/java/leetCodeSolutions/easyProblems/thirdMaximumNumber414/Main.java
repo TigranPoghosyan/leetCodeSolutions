ba18a3b502bc4a126d9c1bdb12d063bbedde8b80
Tigran Poghosyan <yerevan7971@gmail.com>
2024-02-28 21:05:19 +0400

    414. Third Maximum Number

---
+++ src/main/java/leetCodeSolutions/easyProblems/thirdMaximumNumber414/Main.java
@@ -0,0 +1,10 @@
+package leetCodeSolutions.easyProblems.thirdMaximumNumber414;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {3, 2, 1};
+        int result = solution.thirdMax(nums);
+        System.out.println(result);
+    }
+}
