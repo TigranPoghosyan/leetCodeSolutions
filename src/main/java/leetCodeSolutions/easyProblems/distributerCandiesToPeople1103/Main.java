cfda4c7a80b3168ee78638b7d5e8e7fe5de12da1
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-01 19:37:21 +0400

    1103. Distribute Candies to People

---
+++ src/main/java/leetCodeSolutions/easyProblems/distributerCandiesToPeople1103/Main.java
@@ -0,0 +1,11 @@
+package leetCodeSolutions.easyProblems.distributerCandiesToPeople1103;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] result = solution.distributeCandies(7, 4);
+        System.out.println(Arrays.toString(result));
+    }
+}
---
+++ src/main/java/leetCodeSolutions/easyProblems/distributerCandiesToPeople1103/Solution.java
@@ -0,0 +1,7 @@
+package leetCodeSolutions.easyProblems.distributerCandiesToPeople1103;
+
+public class Solution {
+    public int[] distributeCandies(int candies,int numPeople){
+
+    }
+}
