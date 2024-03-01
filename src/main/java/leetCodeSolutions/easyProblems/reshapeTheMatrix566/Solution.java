7005d61c523b891399f86b56c42914d49a63af75
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-01 20:23:06 +0400

    566. Reshape the Matrix

--- src/main/java/leetCodeSolutions/easyProblems/reshapeTheMatrix566/Solution.java
+++ src/main/java/leetCodeSolutions/easyProblems/reshapeTheMatrix566/Solution.java
@@ -1,7 +1,26 @@
 package leetCodeSolutions.easyProblems.reshapeTheMatrix566;
 
-public class Solution {
-    public int[][] matrixReshape(int[][] nums,int r,int c){
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int rows = mat.length;
+        int columns = mat[0].length;
+        if(rows * columns != r * c){
+            return mat;
+        }
+        int[][] result = new int[r][c];
+        int rowNum = 0;
+        int columnNum = 0;
 
+        for (int[] ints : mat) {
+            for (int j = 0; j < columns; j++) {
+                result[rowNum][columnNum] = ints[j];
+                columnNum++;
+                if (columnNum == c) {
+                    columnNum = 0;
+                    rowNum++;
+                }
+            }
+        }
+        return result;
     }
 }
