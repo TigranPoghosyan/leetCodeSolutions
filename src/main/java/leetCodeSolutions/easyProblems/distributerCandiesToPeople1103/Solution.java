97309aeb5631648af45f0cd9db980de082692cf4
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-01 19:58:47 +0400

    1103. Distribute Candies to People

--- src/main/java/leetCodeSolutions/easyProblems/distributerCandiesToPeople1103/Solution.java
+++ src/main/java/leetCodeSolutions/easyProblems/distributerCandiesToPeople1103/Solution.java
@@ -1,7 +1,12 @@
 package leetCodeSolutions.easyProblems.distributerCandiesToPeople1103;
 
 public class Solution {
-    public int[] distributeCandies(int candies,int numPeople){
-
+    public int[] distributeCandies(int candies, int numPeople) {
+        int[] result = new int[numPeople];
+        for (int i = 0; candies > 0; i++) {
+            result[i % numPeople] += Math.min(candies, i + 1);
+            candies -= i + 1;
+        }
+        return result;
     }
 }
