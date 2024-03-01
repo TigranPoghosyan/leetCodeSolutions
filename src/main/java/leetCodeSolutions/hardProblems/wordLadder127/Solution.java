306ffb5dd9b27113bb33ace85a1523ccafbd8615
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-01 22:06:15 +0400

    127. Word Ladder

--- src/main/java/leetCodeSolutions/hardProblems/wordLadder127/Solution.java
+++ src/main/java/leetCodeSolutions/hardProblems/wordLadder127/Solution.java
@@ -1,9 +1,44 @@
 package leetCodeSolutions.hardProblems.wordLadder127;
 
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
-public class Solution {
+class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        HashSet<String> set = new HashSet<>(wordList);
+        if (!set.contains(endWord)) {
+            return 0;
+        }
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        int level = 1;
 
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String currentWord = queue.poll();
+                char[] wordChars = currentWord.toCharArray();
+                for (int j = 0; j < wordChars.length; j++) {
+                    char originalChar = wordChars[j];
+                    for (char c = 'a'; c < 'z'; c++) {
+                        if (wordChars[j] == c) continue;
+                        wordChars[j] = c;
+                        String newWord = String.valueOf(wordChars);
+                        if (newWord.equals(endWord)) {
+                            return level + 1;
+                        }
+                        if (set.contains(newWord)) {
+                            queue.offer(newWord);
+                            set.remove(newWord);
+                        }
+                    }
+                    wordChars[j] = originalChar;
+                }
+            }
+            level++;
+        }
+        return 0;
     }
 }
