f5ec9326002222a7da21a4bacaadb144c9c749b2
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-01 21:18:53 +0400

    720. Longest Word in Dictionary

--- src/main/java/leetCodeSolutions/mediumProblems/longestWordInDictionary720/Main.java
+++ src/main/java/leetCodeSolutions/mediumProblems/longestWordInDictionary720/Main.java
@@ -4,7 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
         String[] words = {"w", "wo", "wor", "worl", "world"};
-        String result = solution.longestWord(words);
-        System.out.println(result);
+        String[] words2 = {"a", "banana", "app", "appl", "ap", "apply", "apple"};
+//        String result = solution.longestWord(words);
+        String result2 = solution.longestWord(words2);
+//        System.out.println(result);
+        System.out.println(result2);
     }
 }
--- src/main/java/leetCodeSolutions/mediumProblems/longestWordInDictionary720/Solution.java
+++ src/main/java/leetCodeSolutions/mediumProblems/longestWordInDictionary720/Solution.java
@@ -1,7 +1,45 @@
 package leetCodeSolutions.mediumProblems.longestWordInDictionary720;
 
-public class Solution {
-    public String longestWord(String[] words){
+import java.util.*;
 
+public class Solution {
+    public String longestWord(String[] words) {
+        Set<String> set = new HashSet<>();
+        Collections.addAll(set, words);
+
+        Queue<String> queue = new LinkedList<>();
+        queue.add("");
+        String word = "";
+
+        while (!queue.isEmpty()) {
+            word = queue.poll();
+            for (int i = 25; i >= 0; i--) {
+                String key = word + (char) ('a' + i);
+                if (set.contains(key)) {
+                    queue.add(key);
+                }
+            }
+        }
+        return word;
     }
 }
+
+//public String longestWord(String[] words) {
+//    Set<String> set = new HashSet<>();
+//    Collections.addAll(set, words);
+//
+//    Queue<String> queue = new LinkedList<>();
+//    queue.add("");
+//    String word = "";
+//
+//    while (!queue.isEmpty()) {
+//        word = queue.poll();
+//        for (int i = 25; i >= 0; i--) {
+//            String key = word + (char) ('a' + i);
+//            if (set.contains(key)) {
+//                queue.add(key);
+//            }
+//        }
+//    }
+//    return word;
+//}
