4869036be557b4fcc88dbb594cfe61123c500663
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-02 16:49:46 +0400

    392. Is Subsequence

--- src/main/java/leetCodeSolutions/easyProblems/isSubsequence392/Solution.java
+++ src/main/java/leetCodeSolutions/easyProblems/isSubsequence392/Solution.java
@@ -2,6 +2,21 @@ package leetCodeSolutions.easyProblems.isSubsequence392;
 
 public class Solution {
     public boolean isSubsequence(String s, String t) {
+        if(s.isEmpty()){
+            return true;
+        }
+        int sPointer = 0;
+        int tPointer = 0;
 
+        for(int i = 0;i<t.length();i++){
+            if(t.charAt(tPointer) == s.charAt(sPointer)){
+                sPointer++;
+                if(sPointer == s.length()){
+                    return true;
+                }
+            }
+            tPointer++;
+        }
+        return false;
     }
 }
