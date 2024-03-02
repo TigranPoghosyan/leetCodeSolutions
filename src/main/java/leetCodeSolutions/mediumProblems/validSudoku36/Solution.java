12c8df866f642b94eb3c3709412da98115e8659d
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-02 20:47:25 +0400

    36. Valid Sudoku

--- src/main/java/leetCodeSolutions/mediumProblems/validSudoku36/Solution.java
+++ src/main/java/leetCodeSolutions/mediumProblems/validSudoku36/Solution.java
@@ -1,7 +1,27 @@
 package leetCodeSolutions.mediumProblems.validSudoku36;
 
-public class Solution {
-    public boolean isValidSudoku(char[][] board){
+import java.util.HashSet;
 
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> seen = new HashSet<>();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char currentVal = board[i][j];
+                if (currentVal != '.') {
+                    // Construct unique identifiers for each cell value in row, column, and sub-box
+                    String rowIdentifier = currentVal + " found in row " + i;
+                    String columnIdentifier = currentVal + " found in column " + j;
+                    String subBoxIdentifier = currentVal + " found in sub box " + i / 3 + "-" + j / 3;
+
+                    // Check if the identifiers already exist in the HashSet
+                    if (!seen.add(rowIdentifier) || !seen.add(columnIdentifier) || !seen.add(subBoxIdentifier)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
