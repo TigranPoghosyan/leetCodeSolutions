56b1df09956e0bce3e6c7b24baaae6e7c1b8808d
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-02 20:50:59 +0400

    198. House Robber

---
+++ src/main/java/leetCodeSolutions/mediumProblems/houseRobber198/Main.java
@@ -0,0 +1,10 @@
+package leetCodeSolutions.mediumProblems.houseRobber198;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 2, 3, 1};
+        int result = solution.rob(nums);
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/leetCodeSolutions/mediumProblems/houseRobber198/Solution.java
@@ -0,0 +1,7 @@
+package leetCodeSolutions.mediumProblems.houseRobber198;
+
+public class Solution {
+    public int rob(int[] nums){
+
+    }
+}
