837faa2be4f578600bcfb6c63d92eb783cbdc9ce
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-02 16:48:55 +0400

    392. Is Subsequence

---
+++ src/main/java/leetCodeSolutions/easyProblems/isSubsequence392/Main.java
@@ -0,0 +1,9 @@
+package leetCodeSolutions.easyProblems.isSubsequence392;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        boolean isSubsequence = solution.isSubsequence("abc", "ahbgdc");
+        System.out.println(isSubsequence);
+    }
+}
---
+++ src/main/java/leetCodeSolutions/easyProblems/isSubsequence392/Solution.java
@@ -0,0 +1,7 @@
+package leetCodeSolutions.easyProblems.isSubsequence392;
+
+public class Solution {
+    public boolean isSubsequence(String s, String t) {
+
+    }
+}
