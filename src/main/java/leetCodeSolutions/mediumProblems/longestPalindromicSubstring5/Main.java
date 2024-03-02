5ba8c0bd4ca9c77344ade1b8d27f912514ee749f
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-02 16:52:33 +0400

    5. Longest Palindromic Substring

---
+++ src/main/java/leetCodeSolutions/mediumProblems/longestPalindromicSubstring5/Main.java
@@ -0,0 +1,9 @@
+package leetCodeSolutions.mediumProblems.longestPalindromicSubstring5;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String result = solution.longestPalindrome("babad");
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/leetCodeSolutions/mediumProblems/longestPalindromicSubstring5/Solution.java
@@ -0,0 +1,7 @@
+package leetCodeSolutions.mediumProblems.longestPalindromicSubstring5;
+
+public class Solution {
+    public String longestPalindrome(String s){
+
+    }
+}
