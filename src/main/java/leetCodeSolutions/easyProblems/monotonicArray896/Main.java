4e544abd1e740f645fa4ef95a920a326e2b0a1c5
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-02 20:11:24 +0400

    896. Monotonic Array

---
+++ src/main/java/leetCodeSolutions/easyProblems/monotonicArray896/Main.java
@@ -0,0 +1,10 @@
+package leetCodeSolutions.easyProblems.monotonicArray896;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1,2,2,3};
+        boolean result = solution.isMonotonic(nums);
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/leetCodeSolutions/easyProblems/monotonicArray896/Solution.java
@@ -0,0 +1,7 @@
+package leetCodeSolutions.easyProblems.monotonicArray896;
+
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+
+    }
+}
