669cd774cfb5ccaa69b1aa1b2b9933dfa3976272
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-02 20:17:12 +0400

    896. Monotonic Array

--- src/main/java/leetCodeSolutions/easyProblems/monotonicArray896/Solution.java
+++ src/main/java/leetCodeSolutions/easyProblems/monotonicArray896/Solution.java
@@ -2,6 +2,16 @@ package leetCodeSolutions.easyProblems.monotonicArray896;
 
 class Solution {
     public boolean isMonotonic(int[] nums) {
-
+        boolean increasing = true;
+        boolean decreasing = true;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                increasing = false;
+            }
+            if (nums[i] < nums[i + 1]) {
+                decreasing = false;
+            }
+        }
+        return increasing || decreasing;
     }
 }
