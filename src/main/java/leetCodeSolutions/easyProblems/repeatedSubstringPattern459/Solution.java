4f5398350edd2a9f2d84838b02d4a534db5951fd
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-02 18:01:44 +0400

    459. Repeated Substring Pattern

--- src/main/java/leetCodeSolutions/easyProblems/repeatedSubstringPattern459/Solution.java
+++ src/main/java/leetCodeSolutions/easyProblems/repeatedSubstringPattern459/Solution.java
@@ -1,7 +1,8 @@
 package leetCodeSolutions.easyProblems.repeatedSubstringPattern459;
 
 public class Solution {
-    public boolean repeatedSubstringPattern(String s){
-
+    public boolean repeatedSubstringPattern(String s) {
+       int index = (s+s).indexOf(s,1);
+       return index < s.length();
     }
 }
