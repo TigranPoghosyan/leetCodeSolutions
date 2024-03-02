39466bb4607b5ce45c1ae01a696d0d17d03935fa
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-02 22:00:44 +0400

    22. Generate Parentheses

--- src/main/java/leetCodeSolutions/mediumProblems/generateParentheses22/Solution.java
+++ src/main/java/leetCodeSolutions/mediumProblems/generateParentheses22/Solution.java
@@ -1,9 +1,27 @@
 package leetCodeSolutions.mediumProblems.generateParentheses22;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-    public List<String> generateParenthesis(int n){
-
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrace(result, "", 0, 0, n);
+        return result;
     }
+
+    private void backtrace(List<String> result, String currentWord, int open, int close, int max) {
+        if (currentWord.length() == max * 2) {
+            result.add(currentWord);
+            return;
+        }
+        if (open < max) {
+            backtrace(result, currentWord + "(", open + 1, close, max);
+        }
+        if (close < open) {
+            backtrace(result, currentWord + ")", open, close + 1, max);
+
+        }
+    }
+
 }
