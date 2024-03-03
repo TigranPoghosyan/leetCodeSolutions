6aec83d1002b6b5cb350d54d0c58b86439cd1e85
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-03 18:41:55 +0400

    1249. Minimum Remove to Make Valid Parentheses

--- src/main/java/leetCodeSolutions/mediumProblems/minimumRemoveToMakeValidParentheses1249/Main.java
+++ src/main/java/leetCodeSolutions/mediumProblems/minimumRemoveToMakeValidParentheses1249/Main.java
@@ -2,6 +2,8 @@ package leetCodeSolutions.mediumProblems.minimumRemoveToMakeValidParentheses1249
 
 public class Main {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        String result = solution.minRemoveToMakeValid("lee(t(c)o)de)");
+        System.out.println(result);
     }
 }
--- src/main/java/leetCodeSolutions/mediumProblems/minimumRemoveToMakeValidParentheses1249/Solution.java
+++ src/main/java/leetCodeSolutions/mediumProblems/minimumRemoveToMakeValidParentheses1249/Solution.java
@@ -1,7 +1,29 @@
 package leetCodeSolutions.mediumProblems.minimumRemoveToMakeValidParentheses1249;
 
 public class Solution {
-    public String minRemoveToMakeValid(String s){
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder sb = new StringBuilder();
+        int open = 0;
 
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                open++;
+            } else if (c == ')') {
+                if (open == 0) {
+                    continue;
+                }
+                open--;
+            }
+            sb.append(c);
+        }
+        StringBuilder result = new StringBuilder();
+
+        for (int i = sb.length() - 1; i >= 0; i--) {
+            if (sb.charAt(i) == '(' && open-- > 0) {
+                continue;
+            }
+            result.append(sb.charAt(i));
+        }
+        return result.reverse().toString();
     }
 }
