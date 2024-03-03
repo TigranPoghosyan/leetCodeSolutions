e0cf871b2e2a8cb90c2bc09ccf19c0f5c04681ac
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-03 18:19:27 +0400

    775. Global and Local Inversions

--- src/main/java/leetCodeSolutions/mediumProblems/globalAndLocalInversions775/Main.java
+++ src/main/java/leetCodeSolutions/mediumProblems/globalAndLocalInversions775/Main.java
@@ -2,6 +2,9 @@ package leetCodeSolutions.mediumProblems.globalAndLocalInversions775;
 
 public class Main {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        int[] nums = {1, 0, 2};
+        boolean idealPermutation = solution.isIdealPermutation(nums);
+        System.out.println(idealPermutation);
     }
 }
