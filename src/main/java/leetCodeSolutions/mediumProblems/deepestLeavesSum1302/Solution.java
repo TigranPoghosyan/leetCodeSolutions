dd6680a528d0777e8f52556f356aa60ff417cd03
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-04 14:45:25 +0400

    1302. Deepest Leaves Sum

--- src/main/java/leetCodeSolutions/mediumProblems/deepestLeavesSum1302/Solution.java
+++ src/main/java/leetCodeSolutions/mediumProblems/deepestLeavesSum1302/Solution.java
@@ -1,7 +1,30 @@
 package leetCodeSolutions.mediumProblems.deepestLeavesSum1302;
 
-public class Solution {
-    public int deepestLeavesSum(TreeNode root){
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class Solution {
+    public int deepestLeavesSum(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        int levelSum = 0;
+
+        while (!queue.isEmpty()) {
+            levelSum = 0;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                levelSum += current.val;
+
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+        }
+        return levelSum;
     }
 }
