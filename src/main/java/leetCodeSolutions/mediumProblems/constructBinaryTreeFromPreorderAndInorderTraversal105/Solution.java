3978d016caf78d5b5460d90f7fc767a6549883a6
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-04 17:23:17 +0400

    105. Construct Binary Tree from Preorder and Inorder Traversal

--- src/main/java/leetCodeSolutions/mediumProblems/constructBinaryTreeFromPreorderAndInorderTraversal105/Solution.java
+++ src/main/java/leetCodeSolutions/mediumProblems/constructBinaryTreeFromPreorderAndInorderTraversal105/Solution.java
@@ -1,7 +1,22 @@
 package leetCodeSolutions.mediumProblems.constructBinaryTreeFromPreorderAndInorderTraversal105;
 
-public class Solution {
-    public TreeNode buildTree(int[] preorder,int[] inorder){
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return helper(0,0,inorder.length-1,preorder,inorder);
+    }
+    public TreeNode helper(int preStart, int inStart,int inEnd, int[] preOrder, int[] inOrder){
+        if(preStart > preOrder.length-1 || inStart > inEnd) return null;
 
+        TreeNode root = new TreeNode(preOrder[preStart]);
+        int inIndex = 0;
+        for(int i = inStart;i<=inEnd;i++){
+            if(root.val == inOrder[i]){
+                inIndex = i;
+            }
+        }
+        root.left = helper(preStart+1,inStart,inIndex-1,preOrder,inOrder);
+        root.right = helper(preStart+inIndex-inStart+1,inIndex+1,inEnd,preOrder,inOrder);
+
+        return root;
     }
 }
