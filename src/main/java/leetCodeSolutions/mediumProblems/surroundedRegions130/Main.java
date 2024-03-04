a7fe871044a0c26396c36a9d2e3a1ba23727b941
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-04 18:06:03 +0400

    130. Surrounded Regions

--- src/main/java/leetCodeSolutions/mediumProblems/surroundedRegions130/Main.java
+++ src/main/java/leetCodeSolutions/mediumProblems/surroundedRegions130/Main.java
@@ -1,10 +1,12 @@
 package leetCodeSolutions.mediumProblems.surroundedRegions130;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
         char[][] board = {{'X', 'X', 'X', 'X'}, {'X', 'O', 'O', 'X'}, {'X', 'X', 'O', 'X'}, {'X', 'O', 'X', 'X'}};
         solution.solve(board);
-        System.out.println(board);
+        System.out.println(Arrays.deepToString(board));
     }
 }
--- src/main/java/leetCodeSolutions/mediumProblems/surroundedRegions130/Solution.java
+++ src/main/java/leetCodeSolutions/mediumProblems/surroundedRegions130/Solution.java
@@ -1,7 +1,52 @@
 package leetCodeSolutions.mediumProblems.surroundedRegions130;
 
 public class Solution {
-    public void solve(char[][] board){
+    public void solve(char[][] board) {
+        if (board.length == 0 || board[0].length == 0)
+            return;
+        if (board.length < 2 || board[0].length < 2)
+            return;
+        int rows = board.length;
+        int columns = board[0].length;
+        //Any 'O' connected to a boundary can't be turned to 'X', so ...
+        //Start from first and last column, turn 'O' to '*'.
+        for (int i = 0; i < rows; i++) {
+            if (board[i][0] == 'O')
+                boundaryDFS(board, i, 0);
+            if (board[i][columns - 1] == 'O')
+                boundaryDFS(board, i, columns - 1);
+        }
+        //Start from first and last row, turn '0' to '*'
+        for (int j = 0; j < columns; j++) {
+            if (board[0][j] == 'O')
+                boundaryDFS(board, 0, j);
+            if (board[rows - 1][j] == 'O')
+                boundaryDFS(board, rows - 1, j);
+        }
+        //post-prcessing, turn 'O' to 'X', '*' back to 'O', keep 'X' intact.
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (board[i][j] == 'O')
+                    board[i][j] = 'X';
+                else if (board[i][j] == '*')
+                    board[i][j] = 'O';
+            }
+        }
+    }
 
+    //Use DFS algo to turn internal however boundary-connected 'O' to '*';
+    private void boundaryDFS(char[][] board, int i, int j) {
+        if (i < 0 || i > board.length - 1 || j < 0 || j > board[0].length - 1)
+            return;
+        if (board[i][j] == 'O')
+            board[i][j] = '*';
+        if (i > 1 && board[i - 1][j] == 'O')
+            boundaryDFS(board, i - 1, j);
+        if (i < board.length - 2 && board[i + 1][j] == 'O')
+            boundaryDFS(board, i + 1, j);
+        if (j > 1 && board[i][j - 1] == 'O')
+            boundaryDFS(board, i, j - 1);
+        if (j < board[i].length - 2 && board[i][j + 1] == 'O')
+            boundaryDFS(board, i, j + 1);
     }
 }
