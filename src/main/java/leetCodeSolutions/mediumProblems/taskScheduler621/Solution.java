a450aaccfc7a90bff6809246a396038a567134ff
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-04 16:32:06 +0400

    621. Task Scheduler

--- src/main/java/leetCodeSolutions/mediumProblems/taskScheduler621/Solution.java
+++ src/main/java/leetCodeSolutions/mediumProblems/taskScheduler621/Solution.java
@@ -1,7 +1,20 @@
 package leetCodeSolutions.mediumProblems.taskScheduler621;
 
 public class Solution {
-    public int leastInterval(char[] tasks,int n){
+    public int leastInterval(char[] tasks, int n) {
+        char[] charMap = new char[26];
+        int max = 0;
+        int charCount = 0;
+        for (char c : tasks) {
+            charMap[c - 'A']++;
+            max = Math.max(max, charMap[c - 'A']);
+        }
 
+        for (char c : charMap) {
+            if (c == max) {
+                charCount++;
+            }
+        }
+        return Math.max(tasks.length, (max - 1) * (n + 1) + charCount);
     }
 }
