d5caae61441ed967a9c653bd0632dafd9c2009ad
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-04 18:39:41 +0400

    1222. Queens That Can Attack the King

--- src/main/java/leetCodeSolutions/mediumProblems/queesThatCanAttackTheKing1222/Main.java
+++ src/main/java/leetCodeSolutions/mediumProblems/queesThatCanAttackTheKing1222/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Solution solution = new Solution();
         int[][] queens = {{0, 1}, {1, 0}, {4, 0}, {0, 4}, {3, 3}, {2, 4}};
         int[] king = {0, 0};
-        List<List<Integer>> result = solution.queesAttackTheKing(queens, king);
+        List<List<Integer>> result = solution.queensAttacktheKing(queens, king);
         System.out.println(result);
     }
 }
--- src/main/java/leetCodeSolutions/mediumProblems/queesThatCanAttackTheKing1222/Solution.java
+++ src/main/java/leetCodeSolutions/mediumProblems/queesThatCanAttackTheKing1222/Solution.java
@@ -1,9 +1,35 @@
 package leetCodeSolutions.mediumProblems.queesThatCanAttackTheKing1222;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
-    public List<List<Integer>> queesAttackTheKing(int[][] quees,int[] king){
+    public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
+        List<List<Integer>> result = new ArrayList<>();
+        boolean[][] seen = new boolean[8][8];
+        for(int[] queen:queens){
+            seen[queen[0]][queen[1]] = true;
+        }
+        int[] directions = {-1,0,1};
+        for(int dx:directions){
+            for(int dy:directions){
+                if(dx == 0 && dy == 0) continue;
 
+                int x = king[0];
+                int y = king[1];
+
+                while(x+dx >= 0 && x+dx < 8 && y+dy >= 0 && y+dy<8){
+                    x+=dx;
+                    y+=dy;
+
+                    if(seen[x][y]){
+                        result.add(Arrays.asList(x,y));
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
     }
 }
