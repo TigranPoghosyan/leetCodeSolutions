fa826ca7283505a22bf6a5ccbece4e4fce1bfada
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-04 14:18:19 +0400

    713. Subarray Product Less Than K

--- src/main/java/leetCodeSolutions/mediumProblems/subarrayProductLessThanK713/Main.java
+++ src/main/java/leetCodeSolutions/mediumProblems/subarrayProductLessThanK713/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] nums = {10, 5, 2, 6};
-        int result = solution.numSubarrayProductLessThanK(nums, 8);
+        int result = solution.numSubarrayProductLessThanK(nums, 100);
         System.out.println(result);
     }
 }
