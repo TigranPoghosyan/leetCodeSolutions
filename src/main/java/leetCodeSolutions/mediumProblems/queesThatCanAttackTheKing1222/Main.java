0fd6edce3079d0f64bdf3617a81f4dc424f3cf29
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-04 18:10:46 +0400

    1222. Queens That Can Attack the King

---
+++ src/main/java/leetCodeSolutions/mediumProblems/queesThatCanAttackTheKing1222/Main.java
@@ -0,0 +1,13 @@
+package leetCodeSolutions.mediumProblems.queesThatCanAttackTheKing1222;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] queens = {{0, 1}, {1, 0}, {4, 0}, {0, 4}, {3, 3}, {2, 4}};
+        int[] king = {0, 0};
+        List<List<Integer>> result = solution.queesAttackTheKing(queens, king);
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/leetCodeSolutions/mediumProblems/queesThatCanAttackTheKing1222/Solution.java
@@ -0,0 +1,9 @@
+package leetCodeSolutions.mediumProblems.queesThatCanAttackTheKing1222;
+
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> queesAttackTheKing(int[][] quees,int[] king){
+
+    }
+}
