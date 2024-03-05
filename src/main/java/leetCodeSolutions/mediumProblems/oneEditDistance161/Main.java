7b1c08c322574fa0d2d8066f47a6d90c401bfc14
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-05 18:27:22 +0400

    161. One Edit Distance

---
+++ src/main/java/leetCodeSolutions/mediumProblems/oneEditDistance161/Main.java
@@ -0,0 +1,9 @@
+package leetCodeSolutions.mediumProblems.oneEditDistance161;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        boolean result = solution.isOneEditDistance("12178", "1217");
+        System.out.println(result);
+    }
+}
