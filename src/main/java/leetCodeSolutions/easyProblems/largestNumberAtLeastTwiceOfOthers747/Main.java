2d4516b3910d7133d6e8b00efe07540c8a55176f
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-07 17:51:44 +0400

    747. Largest Number At Least Twice of Others

--- src/main/java/leetCodeSolutions/easyProblems/largestNumberAtLeastTwiceOfOthers747/Main.java
+++ src/main/java/leetCodeSolutions/easyProblems/largestNumberAtLeastTwiceOfOthers747/Main.java
@@ -2,6 +2,9 @@ package leetCodeSolutions.easyProblems.largestNumberAtLeastTwiceOfOthers747;
 
 public class Main {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        int[] nums = {3, 6, 1, 0};
+        int result = solution.dominantIndex(nums);
+        System.out.println(result);
     }
 }
--- src/main/java/leetCodeSolutions/easyProblems/largestNumberAtLeastTwiceOfOthers747/Solution.java
+++ src/main/java/leetCodeSolutions/easyProblems/largestNumberAtLeastTwiceOfOthers747/Solution.java
@@ -1,5 +1,22 @@
 package leetCodeSolutions.easyProblems.largestNumberAtLeastTwiceOfOthers747;
 
-public class Solution {
+class Solution {
+    public int dominantIndex(int[] nums) {
+        int max = 0;
+        for (int num : nums) {
+            max = Math.max(max, num);
+        }
+        int position = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] == max){
+                position = i;
+            }
+            int twice = nums[i] * 2;
 
+            if(twice > max && nums[i] != max){
+                return -1;
+            }
+        }
+        return position;
+    }
 }
