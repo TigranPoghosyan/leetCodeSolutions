6dd91a529bfaea7d3fd08557e8e36a3ccf1f69f4
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-07 16:23:27 +0400

    744. Find Smallest Letter Greater Than Target

--- src/main/java/leetCodeSolutions/easyProblems/findSmallesLetterGraterThanTarget744/Main.java
+++ src/main/java/leetCodeSolutions/easyProblems/findSmallesLetterGraterThanTarget744/Main.java
@@ -2,6 +2,9 @@ package leetCodeSolutions.easyProblems.findSmallesLetterGraterThanTarget744;
 
 public class Main {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        char[] letters = {'c', 'f', 'j'};
+        char result = solution.nextGreatestLetter(letters, 'a');
+        System.out.println(result);
     }
 }
