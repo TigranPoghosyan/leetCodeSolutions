6acfa639ba7fc2bdc978ee35ddda9252891db8bc
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-07 17:19:48 +0400

    3069. Distribute Elements Into Two Arrays I

--- src/main/java/leetCodeSolutions/easyProblems/distributeElementsIntoTwoArraysFirst3069/Solution.java
+++ src/main/java/leetCodeSolutions/easyProblems/distributeElementsIntoTwoArraysFirst3069/Solution.java
@@ -2,6 +2,26 @@ package leetCodeSolutions.easyProblems.distributeElementsIntoTwoArraysFirst3069;
 
 public class Solution {
     public int[] resultArray(int[] nums) {
+        int first = 0;
+        int current = 0;
+        int temp = 0;
+        int second = 1;
 
+        while (second < nums.length - 1) {
+            if (nums[first] > nums[second]) {
+                current = second;
+                while (first != second) {
+                    temp = nums[second];
+                    nums[second] = nums[second + 1];
+                    nums[second + 1] = temp;
+                    second--;
+                }
+                first++;
+                second = current;
+            }
+            second++;
+        }
+
+        return nums;
     }
 }
