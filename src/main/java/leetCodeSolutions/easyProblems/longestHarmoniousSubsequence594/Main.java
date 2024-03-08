f63f3efae21dcf75f9a6e168b4b5056ab8c24cb6
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-08 15:35:46 +0400

    594. Longest Harmonious Subsequence

---
+++ src/main/java/leetCodeSolutions/easyProblems/longestHarmoniousSubsequence594/Main.java
@@ -0,0 +1,10 @@
+package leetCodeSolutions.easyProblems.longestHarmoniousSubsequence594;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 3, 2, 2, 5, 2, 3, 7};
+        int result = solution.findLHS(nums);
+        System.out.println(result);
+    }
+}
