ef6ef2ebdb9c087d99fda6c0a06cf86740754413
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-09 21:02:48 +0400

    599. Minimum Index Sum of Two Lists

--- src/main/java/leetCodeSolutions/easyProblems/minimumIndexOfTwoLists599/Main.java
+++ src/main/java/leetCodeSolutions/easyProblems/minimumIndexOfTwoLists599/Main.java
@@ -1,8 +1,13 @@
 package leetCodeSolutions.easyProblems.minimumIndexOfTwoLists599;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-
+        String[] list1 = {"happy","sad","good"};
+        String[] list2 = {"sad","happy","good"};
+        String[] result = solution.findRestaurant(list1, list2);
+        System.out.println(Arrays.toString(result));
     }
 }
--- src/main/java/leetCodeSolutions/easyProblems/minimumIndexOfTwoLists599/Solution.java
+++ src/main/java/leetCodeSolutions/easyProblems/minimumIndexOfTwoLists599/Solution.java
@@ -1,7 +1,32 @@
 package leetCodeSolutions.easyProblems.minimumIndexOfTwoLists599;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Solution {
     public String[] findRestaurant(String[] list1, String[] list2) {
 
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < list1.length; i++) {
+            map.put(list1[i], i);
+        }
+
+        int min = Integer.MAX_VALUE;
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < list2.length; i++) {
+            if (map.containsKey(list2[i])) {
+                int sum = map.get(list2[i]) + i;
+                if (sum < min) {
+                    min = sum;
+                    res.clear();
+                    res.add(list2[i]);
+                } else if (min == sum) {
+                    res.add(list2[i]);
+                }
+            }
+        }
+        return res.toArray(new String[]{});
     }
 }
