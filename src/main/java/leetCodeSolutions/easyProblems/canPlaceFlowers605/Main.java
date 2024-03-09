b7d06e441f7f53eb64f18a5ed7fe7e18ab71eb2e
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-09 20:21:08 +0400

    605. Can Place Flowers

--- src/main/java/leetCodeSolutions/easyProblems/canPlaceFlowers605/Main.java
+++ src/main/java/leetCodeSolutions/easyProblems/canPlaceFlowers605/Main.java
@@ -2,6 +2,9 @@ package leetCodeSolutions.easyProblems.canPlaceFlowers605;
 
 public class Main {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        int[] flowerBed = {1,0,0,0,1};
+        boolean result = solution.canPlaceFlowers(flowerBed,2);
+        System.out.println(result);
     }
 }
--- src/main/java/leetCodeSolutions/easyProblems/canPlaceFlowers605/Solution.java
+++ src/main/java/leetCodeSolutions/easyProblems/canPlaceFlowers605/Solution.java
@@ -1,5 +1,22 @@
 package leetCodeSolutions.easyProblems.canPlaceFlowers605;
 
-public class Solution {
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if (n == 0) {
+            return true;
+        }
+        int count = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                int prev = (i == 0 || flowerbed[i - 1] == 0) ? 0 : 1;
+                int next = (i == flowerbed.length - 1 || flowerbed[i + 1] == 0) ? 0 : 1;
 
+                if (prev == 0 && next == 0) {
+                    flowerbed[i] = 1;
+                    count++;
+                }
+            }
+        }
+        return count >= n;
+    }
 }
