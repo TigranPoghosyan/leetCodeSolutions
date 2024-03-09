777d48f8467739a7d81fe208093afd9e3ebcc41b
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-09 19:49:09 +0400

    2540. Minimum Common Value

---
+++ src/main/java/leetCodeSolutions/easyProblems/minimumCommonValue2540/Main.java
@@ -0,0 +1,7 @@
+package leetCodeSolutions.easyProblems.minimumCommonValue2540;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/leetCodeSolutions/easyProblems/minimumCommonValue2540/Solution.java
@@ -0,0 +1,7 @@
+package leetCodeSolutions.easyProblems.minimumCommonValue2540;
+
+public class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+
+    }
+}
