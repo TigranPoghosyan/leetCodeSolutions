fead2023e6c204479aa9011253cb7b97bf82f009
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-09 19:58:04 +0400

    2540. Minimum Common Value

--- src/main/java/leetCodeSolutions/easyProblems/minimumCommonValue2540/Main.java
+++ src/main/java/leetCodeSolutions/easyProblems/minimumCommonValue2540/Main.java
@@ -2,6 +2,10 @@ package leetCodeSolutions.easyProblems.minimumCommonValue2540;
 
 public class Main {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        int[] nums1 = {1, 2, 3, 4};
+        int[] nums2 = {2, 4, 5};
+        int result = solution.getCommon(nums1, nums2);
+        System.out.println(result);
     }
 }
--- src/main/java/leetCodeSolutions/easyProblems/minimumCommonValue2540/Solution.java
+++ src/main/java/leetCodeSolutions/easyProblems/minimumCommonValue2540/Solution.java
@@ -1,7 +1,19 @@
 package leetCodeSolutions.easyProblems.minimumCommonValue2540;
 
-public class Solution {
+class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
+        int firstStart = 0;
+        int secondStart = 0;
 
+        while (firstStart < nums1.length && secondStart < nums2.length) {
+            if (nums1[firstStart] == nums2[secondStart]) {
+                return nums1[firstStart];
+            } else if (nums1[firstStart] > nums2[secondStart]) {
+                secondStart++;
+            } else {
+                firstStart++;
+            }
+        }
+        return -1;
     }
 }
