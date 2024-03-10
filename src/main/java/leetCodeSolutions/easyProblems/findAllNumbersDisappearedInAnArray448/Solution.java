2fb33a2916b685981a478e4170849466756cd284
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-10 23:18:16 +0400

    448. Find All Numbers Disappeared in an Array

--- src/main/java/leetCodeSolutions/easyProblems/findAllNumbersDisappearedInAnArray448/Solution.java
+++ src/main/java/leetCodeSolutions/easyProblems/findAllNumbersDisappearedInAnArray448/Solution.java
@@ -11,12 +11,12 @@ public class Solution {
                 nums[index - 1] *= -1;
             }
         }
-        List<Integer> res = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                res.add(i + 1);
-            }
-        }
+//        List<Integer> res = new ArrayList<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            if (nums[i] > 0) {
+//                res.add(i + 1);
+//            }
+//        }
         return res;
     }
 }
