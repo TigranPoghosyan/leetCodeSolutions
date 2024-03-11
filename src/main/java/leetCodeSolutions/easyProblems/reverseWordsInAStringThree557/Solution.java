61f170b15974f05c97ba4143e6cb13e0643f035b
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-11 17:11:41 +0400

    557. Reverse Words in a String III

--- src/main/java/leetCodeSolutions/easyProblems/reverseWordsInAStringThree557/Solution.java
+++ src/main/java/leetCodeSolutions/easyProblems/reverseWordsInAStringThree557/Solution.java
@@ -1,7 +1,15 @@
 package leetCodeSolutions.easyProblems.reverseWordsInAStringThree557;
 
-public class Solution {
+class Solution {
     public String reverseWords(String s) {
-
+        String[] arr = s.split(" ");
+        StringBuilder result = new StringBuilder();
+        for (String word : arr) {
+            for (int i = word.length() - 1; i >= 0; i--) {
+                result.append(word.charAt(i));
+            }
+            result.append(" ");
+        }
+        return result.toString().trim();
     }
 }
