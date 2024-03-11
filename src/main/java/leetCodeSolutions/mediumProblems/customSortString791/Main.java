bf1c04eeace2d1d942e20c5d70785a8a746225e1
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-11 15:00:33 +0400

    791. Custom Sort String

--- src/main/java/leetCodeSolutions/mediumProblems/customSortString791/Main.java
+++ src/main/java/leetCodeSolutions/mediumProblems/customSortString791/Main.java
@@ -2,6 +2,8 @@ package leetCodeSolutions.mediumProblems.customSortString791;
 
 public class Main {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        String result = solution.customSortString("kqep","pekeq");
+        System.out.println(result);
     }
 }
--- src/main/java/leetCodeSolutions/mediumProblems/customSortString791/Solution.java
+++ src/main/java/leetCodeSolutions/mediumProblems/customSortString791/Solution.java
@@ -5,27 +5,19 @@ import java.util.HashMap;
 public class Solution {
     public String customSortString(String order, String s) {
         StringBuilder result = new StringBuilder();
-        HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> mp = new HashMap<>();
         for (char c : s.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
+            mp.put(c, mp.getOrDefault(c, 0) + 1);
         }
         for (char c : order.toCharArray()) {
-            if (map.containsKey(c)) {
-                if (map.get(c) > 1) {
-                    map.put(c, map.getOrDefault(c, 0) - 1);
-                    result.append(c);
-                } else {
-                    result.append(c);
-                    map.remove(c);
-                }
+            if (mp.containsKey(c)) {
+                result.append(String.valueOf(c).repeat(Math.max(0, mp.get(c))));
+                mp.remove(c);
             }
         }
-
-        for (char c : map.keySet()) {
-            result.append(String.valueOf(c));
-            map.remove(c);
+        for (char c : mp.keySet()) {
+            result.append(String.valueOf(c).repeat(Math.max(0, mp.get(c))));
         }
-
         return result.toString();
     }
 }
