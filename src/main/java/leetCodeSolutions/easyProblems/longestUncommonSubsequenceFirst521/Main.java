4a0603084c50d3ded5cc8febbaa329ccd5d90ef8
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-11 15:37:50 +0400

    521. Longest Uncommon Subsequence I

--- src/main/java/leetCodeSolutions/easyProblems/longestUncommonSubsequenceFirst521/Main.java
+++ src/main/java/leetCodeSolutions/easyProblems/longestUncommonSubsequenceFirst521/Main.java
@@ -4,7 +4,7 @@ package leetCodeSolutions.easyProblems.longestUncommonSubsequenceFirst521;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int result = solution.findLUSlength("aba", "cdc");
+        int result = solution.findLUSLength("aba", "cdc");
         System.out.println(result);
     }
 }
--- src/main/java/leetCodeSolutions/easyProblems/longestUncommonSubsequenceFirst521/Solution.java
+++ src/main/java/leetCodeSolutions/easyProblems/longestUncommonSubsequenceFirst521/Solution.java
@@ -1,7 +1,10 @@
 package leetCodeSolutions.easyProblems.longestUncommonSubsequenceFirst521;
 
 public class Solution {
-    public int findLUSlength(String a, String b) {
-
+    public int findLUSLength(String a, String b) {
+        if (a.equals(b)) {
+            return -1;
+        }
+        return Math.max(a.length(), b.length());
     }
 }
