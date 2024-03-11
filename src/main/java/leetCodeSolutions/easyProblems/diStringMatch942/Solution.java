a2f9554505c93eaaeb0c345650f88a5d0c552b47
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-11 22:03:07 +0400

    942. DI String Match

--- src/main/java/leetCodeSolutions/easyProblems/diStringMatch942/Solution.java
+++ src/main/java/leetCodeSolutions/easyProblems/diStringMatch942/Solution.java
@@ -1,4 +1,22 @@
 package leetCodeSolutions.easyProblems.diStringMatch942;
 
-public class Solution {
+class Solution {
+    public int[] diStringMatch(String s) {
+        int n = s.length();
+        int[] arr = new int[n + 1];
+        char[] c = s.toCharArray();
+        int minNum = 0;
+        int greatNum = n;
+        for(int i = 0;i < n; i++){
+            if(c[i] == 'D'){
+                arr[i] = greatNum;
+                greatNum--;
+            } else {
+                arr[i] = minNum;
+                minNum++;
+            }
+        }
+        arr[n] = greatNum;
+        return arr;
+    }
 }
