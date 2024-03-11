746f006e74ba33a96234e771adf4fcf9bd733905
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-11 15:11:22 +0400

    791. Custom Sort String

--- src/main/java/leetCodeSolutions/mediumProblems/customSortString791/Solution.java
+++ src/main/java/leetCodeSolutions/mediumProblems/customSortString791/Solution.java
@@ -1,23 +1,44 @@
 package leetCodeSolutions.mediumProblems.customSortString791;
 
-import java.util.HashMap;
-
 public class Solution {
     public String customSortString(String order, String s) {
-        StringBuilder result = new StringBuilder();
-        HashMap<Character, Integer> mp = new HashMap<>();
-        for (char c : s.toCharArray()) {
-            mp.put(c, mp.getOrDefault(c, 0) + 1);
+        StringBuilder builder = new StringBuilder();
+        int[] freq = new int[32];
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
         }
-        for (char c : order.toCharArray()) {
-            if (mp.containsKey(c)) {
-                result.append(String.valueOf(c).repeat(Math.max(0, mp.get(c))));
-                mp.remove(c);
+
+        for (int i = 0; i < order.length(); i++) {
+            while (freq[order.charAt(i) - 'a'] > 0) {
+                builder.append(order.charAt(i));
+                freq[order.charAt(i) - 'a']--;
             }
         }
-        for (char c : mp.keySet()) {
-            result.append(String.valueOf(c).repeat(Math.max(0, mp.get(c))));
+        for (int i = 0; i < freq.length; i++) {
+            while (freq[i] > 0) {
+                builder.append((char) (i + 'a'));
+                freq[i]--;
+            }
         }
-        return result.toString();
+        return String.valueOf(builder);
     }
 }
+
+// First Solution
+//public String customSortString(String order, String s) {
+//    StringBuilder result = new StringBuilder();
+//    HashMap<Character, Integer> mp = new HashMap<>();
+//    for (char c : s.toCharArray()) {
+//        mp.put(c, mp.getOrDefault(c, 0) + 1);
+//    }
+//    for (char c : order.toCharArray()) {
+//        if (mp.containsKey(c)) {
+//            result.append(String.valueOf(c).repeat(Math.max(0, mp.get(c))));
+//            mp.remove(c);
+//        }
+//    }
+//    for (char c : mp.keySet()) {
+//        result.append(String.valueOf(c).repeat(Math.max(0, mp.get(c))));
+//    }
+//    return result.toString();
+//}
