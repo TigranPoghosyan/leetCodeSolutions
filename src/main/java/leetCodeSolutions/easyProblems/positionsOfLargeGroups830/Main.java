78a179134fc8afa45fdee2619d94c69c9523d01c
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-11 17:29:05 +0400

    830. Positions of Large Groups

---
+++ src/main/java/leetCodeSolutions/easyProblems/positionsOfLargeGroups830/Main.java
@@ -0,0 +1,11 @@
+package leetCodeSolutions.easyProblems.positionsOfLargeGroups830;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        List<List<Integer>> result = solution.largeGroupPositions("abbxxxxzzy");
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/leetCodeSolutions/easyProblems/positionsOfLargeGroups830/Solution.java
@@ -0,0 +1,9 @@
+package leetCodeSolutions.easyProblems.positionsOfLargeGroups830;
+
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> largeGroupPositions(String s) {
+
+    }
+}
