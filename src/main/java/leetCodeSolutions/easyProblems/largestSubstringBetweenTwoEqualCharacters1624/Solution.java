487027c969246c8345b037bc413fa0dac553d8ee
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-12 18:33:07 +0400

    1624. Largest Substring Between Two Equal Characters

--- src/main/java/leetCodeSolutions/easyProblems/largestSubstringBetweenTwoEqualCharacters1624/Solution.java
+++ src/main/java/leetCodeSolutions/easyProblems/largestSubstringBetweenTwoEqualCharacters1624/Solution.java
@@ -2,6 +2,18 @@ package leetCodeSolutions.easyProblems.largestSubstringBetweenTwoEqualCharacters
 
 public class Solution {
     public int maxLengthBetweenEqualCharacters(String s) {
+        int maxLength = -1;
+        int[] mapCount = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            int index = ch - 'a';
 
+            if (mapCount[index] == 0) {
+                mapCount[index] = i + 1;
+            } else {
+                maxLength = Math.max(maxLength, i - mapCount[index]);
+            }
+        }
+        return maxLength;
     }
 }
