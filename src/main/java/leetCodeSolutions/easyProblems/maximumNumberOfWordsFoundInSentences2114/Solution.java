d9e358cad62d9b7e388c1991ecc8c10e58072168
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-12 18:46:22 +0400

    2114. Maximum Number of Words Found in Sentences

--- src/main/java/leetCodeSolutions/easyProblems/maximumNumberOfWordsFoundInSentences2114/Solution.java
+++ src/main/java/leetCodeSolutions/easyProblems/maximumNumberOfWordsFoundInSentences2114/Solution.java
@@ -2,6 +2,25 @@ package leetCodeSolutions.easyProblems.maximumNumberOfWordsFoundInSentences2114;
 
 class Solution {
     public int mostWordsFound(String[] sentences) {
-
+        int max = 0;
+        for (String sentence : sentences) {
+            // max = Math.max(max,sentence.split(" ").length);
+            max = Math.max(max, countWord(sentence));
+        }
+        return max;
+    }
+    public int countWord(String s) {
+        int start = 0;
+        int end = s.length();
+        int count = 0;
+        char[] c = s.toCharArray();
+        while (start < end) {
+            if (c[start] == ' ') {
+                count++;
+            }
+            start++;
+        }
+        count++;
+        return count;
     }
 }
