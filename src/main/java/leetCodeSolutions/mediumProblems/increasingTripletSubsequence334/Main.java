10a86b7c1bd447b547e62347aa74bffaa391c4ab
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-12 18:06:17 +0400

    334. Increasing Triplet Subsequence

---
+++ src/main/java/leetCodeSolutions/mediumProblems/increasingTripletSubsequence334/Main.java
@@ -0,0 +1,7 @@
+package leetCodeSolutions.mediumProblems.increasingTripletSubsequence334;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/leetCodeSolutions/mediumProblems/increasingTripletSubsequence334/Solution.java
@@ -0,0 +1,7 @@
+package leetCodeSolutions.mediumProblems.increasingTripletSubsequence334;
+
+public class Solution {
+    public boolean increasingTriplet(int[] nums) {
+
+    }
+}
