2de31a108122236302ac64b2652ebeaa4973929e
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-12 15:23:08 +0400

    155. Min Stack

---
+++ src/main/java/leetCodeSolutions/mediumProblems/minStack155/Main.java
@@ -0,0 +1,15 @@
+package leetCodeSolutions.mediumProblems.minStack155;
+
+public class Main {
+    public static void main(String[] args) {
+//        Solution solution = new Solution();
+//        solution.push(10);
+//        solution.push(5);
+//        solution.push(7);
+//        solution.push(15);
+//        solution.pop();
+//        int top = solution.top();
+//        int min = solution.getMin();
+//        System.out.println("Top Value = " + top + " Min Value = " + min);
+    }
+}
