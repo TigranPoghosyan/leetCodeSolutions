eb996d2dc840d737f98337db4ba89e6ce94e4c52
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-12 15:04:21 +0400

    258. Add Digits

--- src/main/java/leetCodeSolutions/easyProblems/addDigits258/Main.java
+++ src/main/java/leetCodeSolutions/easyProblems/addDigits258/Main.java
@@ -2,6 +2,8 @@ package leetCodeSolutions.easyProblems.addDigits258;
 
 public class Main {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        int result = solution.addDigits(38);
+        System.out.println(result);
     }
 }
--- src/main/java/leetCodeSolutions/easyProblems/addDigits258/Solution.java
+++ src/main/java/leetCodeSolutions/easyProblems/addDigits258/Solution.java
@@ -1,5 +1,10 @@
 package leetCodeSolutions.easyProblems.addDigits258;
 
 public class Solution {
+    public int addDigits(int num) {
+        if (num == 0) return 0;
+        if (num % 9 == 0) return 9;
 
+        return num % 9;
+    }
 }
