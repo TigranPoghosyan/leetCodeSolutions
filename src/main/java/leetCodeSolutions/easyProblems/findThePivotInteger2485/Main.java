6e41d5c555da19af79f8a8cc3df806ac447cc30b
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-13 14:49:17 +0400

    2485. Find the Pivot Integer

--- src/main/java/leetCodeSolutions/easyProblems/findThePivotInteger2485/Main.java
+++ src/main/java/leetCodeSolutions/easyProblems/findThePivotInteger2485/Main.java
@@ -2,6 +2,8 @@ package leetCodeSolutions.easyProblems.findThePivotInteger2485;
 
 public class Main {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        int result = solution.pivotInteger(8);
+        System.out.println(result);
     }
 }
--- src/main/java/leetCodeSolutions/easyProblems/findThePivotInteger2485/Solution.java
+++ src/main/java/leetCodeSolutions/easyProblems/findThePivotInteger2485/Solution.java
@@ -1,4 +1,13 @@
 package leetCodeSolutions.easyProblems.findThePivotInteger2485;
 
 public class Solution {
+    public int pivotInteger(int n) {
+        int answer = (n * n + n) / 2;
+        int square = (int) Math.sqrt(answer);
+        if (square * square == answer) {
+            return square;
+        } else {
+            return -1;
+        }
+    }
 }
