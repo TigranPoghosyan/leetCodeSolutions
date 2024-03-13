db6baae06f3f0c81aeb53882ca1020e5e5894587
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-13 16:00:58 +0400

    705. Design HashSet

---
+++ src/main/java/leetCodeSolutions/easyProblems/designHashSet705/Main.java
@@ -0,0 +1,16 @@
+package leetCodeSolutions.easyProblems.designHashSet705;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.add(1);      // set = [1]
+        solution.add(2);      // set = [1, 2]
+        solution.contains(1); // return True
+        solution.contains(3); // return False, (not found)
+        solution.add(2);      // set = [1, 2]
+        solution.contains(2); // return True
+        solution.remove(2);   // set = [1]
+        solution.contains(2); // return False, (already removed)
+        System.out.println(solution);
+    }
+}
---
+++ src/main/java/leetCodeSolutions/easyProblems/designHashSet705/Solution.java
@@ -0,0 +1,19 @@
+package leetCodeSolutions.easyProblems.designHashSet705;
+
+public class Solution {
+    public Solution() {
+
+    }
+
+    public void add(int key) {
+
+    }
+
+    public void remove(int key) {
+
+    }
+
+    public boolean contains(int key) {
+
+    }
+}
