a239e880a4fb25f2607aa6c66f326a22a0034642
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-13 15:51:09 +0400

    551. Student Attendance Record I

---
+++ src/main/java/leetCodeSolutions/easyProblems/removeOutermostParentheses1021/Main.java
@@ -0,0 +1,9 @@
+package leetCodeSolutions.easyProblems.removeOutermostParentheses1021;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String result = solution.removeOuterParentheses("(()())(())");
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/leetCodeSolutions/easyProblems/removeOutermostParentheses1021/Solution.java
@@ -0,0 +1,7 @@
+package leetCodeSolutions.easyProblems.removeOutermostParentheses1021;
+
+public class Solution {
+    public String removeOuterParentheses(String S) {
+
+    }
+}
