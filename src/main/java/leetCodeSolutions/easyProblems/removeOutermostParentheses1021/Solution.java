bd1a998c821565b1d5b27719a0b280bb899f7262
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-13 15:54:09 +0400

    1021. Remove Outermost Parentheses

--- src/main/java/leetCodeSolutions/easyProblems/removeOutermostParentheses1021/Solution.java
+++ src/main/java/leetCodeSolutions/easyProblems/removeOutermostParentheses1021/Solution.java
@@ -2,6 +2,21 @@ package leetCodeSolutions.easyProblems.removeOutermostParentheses1021;
 
 public class Solution {
     public String removeOuterParentheses(String S) {
-
+        StringBuilder sb = new StringBuilder();
+        int counter = 0;
+        for (char c : S.toCharArray()) {
+            if (c == '(') {
+                if (counter != 0) {
+                    sb.append(c);
+                }
+                counter++;
+            } else {
+                counter--;
+                if (counter != 0) {
+                    sb.append(c);
+                }
+            }
+        }
+        return sb.toString();
     }
 }
