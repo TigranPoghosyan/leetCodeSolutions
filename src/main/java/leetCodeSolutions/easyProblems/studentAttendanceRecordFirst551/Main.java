20afc82285b64799b38b7a2a2269105eefba746b
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-13 15:32:15 +0400

    551. Student Attendance Record I

---
+++ src/main/java/leetCodeSolutions/easyProblems/studentAttendanceRecordFirst551/Main.java
@@ -0,0 +1,9 @@
+package leetCodeSolutions.easyProblems.studentAttendanceRecordFirst551;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        boolean result = solution.checkRecord("PPALLP");
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/leetCodeSolutions/easyProblems/studentAttendanceRecordFirst551/Solution.java
@@ -0,0 +1,7 @@
+package leetCodeSolutions.easyProblems.studentAttendanceRecordFirst551;
+
+public class Solution {
+    public boolean checkRecord(String s) {
+
+    }
+}
