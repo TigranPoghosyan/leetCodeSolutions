09dacdf6f0ce436c9e341a22bdcd6faeb6791352
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-13 17:10:05 +0400

    73. Set Matrix Zeroes

--- src/main/java/leetCodeSolutions/mediumProblems/setMatrixZeroes73/Solution.java
+++ src/main/java/leetCodeSolutions/mediumProblems/setMatrixZeroes73/Solution.java
@@ -2,6 +2,41 @@ package leetCodeSolutions.mediumProblems.setMatrixZeroes73;
 
 public class Solution {
     public void setZeroes(int[][] matrix) {
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        boolean row = false;
+        boolean column = false;
 
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (matrix[i][j] == 0) {
+                    if (i == 0) {
+                        row = true;
+                    }
+                    if (j == 0) {
+                        column = true;
+                    }
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < columns; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (row) {
+            for (int i = 0; i < rows; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        if (column) {
+            for (int i = 0; i < columns; i++) {
+                matrix[0][i] = 0;
+            }
+        }
     }
 }
