7d012a942f5b29442dd51284596829435b28eecd
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-15 18:31:04 +0400

    90. Subsets II

--- src/main/java/leetCodeSolutions/mediumProblems/subsetsTwo90/Solution.java
+++ src/main/java/leetCodeSolutions/mediumProblems/subsetsTwo90/Solution.java
@@ -5,22 +5,23 @@ import java.util.Arrays;
 import java.util.List;
 
 class Solution {
-    List<List<Integer>> output;
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        output = new ArrayList<>();
-        backTracking(0,nums, new ArrayList<>());
-        return output;
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+        backTracking(0, nums, result, current);
+        return result;
     }
-    public void backTracking(int index, int[] nums, List<Integer> list){
-        output.add(new ArrayList<>(list));
-        for(int i = index;i < nums.length; i++){
-            if(i != index && nums[i] == nums[i-1]){
+
+    public void backTracking(int index, int[] nums, List<List<Integer>> result, List<Integer> current) {
+        result.add(new ArrayList<>(current));
+        for (int i = index; i < nums.length; i++) {
+            if (i != index && nums[i] == nums[i - 1]) {
                 continue;//skip the duplicates, except for the first time
             }
-            list.add(nums[i]); //include
-            backTracking(i+1,nums,list); //explore
-            list.remove(list.size()-1);//backtrack, remove the element
+            current.add(nums[i]); //include
+            backTracking(i + 1, nums, result, current); //explore
+            current.remove(current.size() - 1);//backtrack, remove the element
         }
     }
 }
