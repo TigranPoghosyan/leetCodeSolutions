6c62e967e44f9d1ed7d58c26f627cbd5a1cbc2d0
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-15 18:03:55 +0400

    81. Search in Rotated Sorted Array II

--- src/main/java/leetCodeSolutions/mediumProblems/searchInRotatedSortedArrayTwo81/Solution.java
+++ src/main/java/leetCodeSolutions/mediumProblems/searchInRotatedSortedArrayTwo81/Solution.java
@@ -2,8 +2,8 @@ package leetCodeSolutions.mediumProblems.searchInRotatedSortedArrayTwo81;
 
 public class Solution {
     public boolean search(int[] nums, int target) {
-//        int low = 0;
-//        int high = nums.length - 1;
+        int low = 0;
+        int high = nums.length - 1;
 
         while (low <= high) {
             int mid = (low + high) / 2;
