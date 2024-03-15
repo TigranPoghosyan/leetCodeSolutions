1e0e8c2aecb5bfdbb951b855d37e885f67e93ba1
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-15 16:42:55 +0400

    64. Minimum Path Sum

--- src/main/java/leetCodeSolutions/mediumProblems/minimumPathSum64/Solution.java
+++ src/main/java/leetCodeSolutions/mediumProblems/minimumPathSum64/Solution.java
@@ -2,22 +2,22 @@ package leetCodeSolutions.mediumProblems.minimumPathSum64;
 
 public class Solution {
     public int minPathSum(int[][] grid) {
-//        int rows = grid.length;
-//        int columns = grid[0].length;
-//
-//        for (int i = 1; i < rows; i++) {
-//            grid[i][0] += grid[i-1][0];
-//        }
-//
-//        for (int j = 1; j < columns; j++) {
-//            grid[0][j] += grid[0][j-1];
-//        }
-//
-//        for (int i = 1; i < rows; i++) {
-//            for (int j = 1; j < columns; j++) {
-//                grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
-//            }
-//        }
-//        return grid[rows-1][columns-1];
+        int rows = grid.length;
+        int columns = grid[0].length;
+
+        for (int i = 1; i < rows; i++) {
+            grid[i][0] += grid[i-1][0];
+        }
+
+        for (int j = 1; j < columns; j++) {
+            grid[0][j] += grid[0][j-1];
+        }
+
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < columns; j++) {
+                grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
+            }
+        }
+        return grid[rows-1][columns-1];
     }
 }
