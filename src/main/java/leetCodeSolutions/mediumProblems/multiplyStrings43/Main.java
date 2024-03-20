7c7bdcc5ad6da89b0647697a1a1072763d2792b2
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-20 17:01:00 +0400

    43. Multiply Strings

--- src/main/java/leetCodeSolutions/mediumProblems/multiplyStrings43/Main.java
+++ src/main/java/leetCodeSolutions/mediumProblems/multiplyStrings43/Main.java
@@ -2,6 +2,8 @@ package leetCodeSolutions.mediumProblems.multiplyStrings43;
 
 public class Main {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        String multiply = solution.multiply("2", "3");
+        System.out.println(multiply);
     }
 }
--- src/main/java/leetCodeSolutions/mediumProblems/multiplyStrings43/Solution.java
+++ src/main/java/leetCodeSolutions/mediumProblems/multiplyStrings43/Solution.java
@@ -1,5 +1,7 @@
 package leetCodeSolutions.mediumProblems.multiplyStrings43;
 
 public class Solution {
+    public String multiply(String num1, String num2) {
 
+    }
 }
