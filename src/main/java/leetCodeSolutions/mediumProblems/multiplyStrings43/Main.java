7e8d965784fc2027acbc3faf0d831f47cba12f3e
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-20 16:59:31 +0400

    43. Multiply Strings

---
+++ src/main/java/leetCodeSolutions/mediumProblems/multiplyStrings43/Main.java
@@ -0,0 +1,7 @@
+package leetCodeSolutions.mediumProblems.multiplyStrings43;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/leetCodeSolutions/mediumProblems/multiplyStrings43/Solution.java
@@ -0,0 +1,5 @@
+package leetCodeSolutions.mediumProblems.multiplyStrings43;
+
+public class Solution {
+
+}
