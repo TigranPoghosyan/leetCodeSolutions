32d15f08f415dfc61f65faa0521461a33a3388be
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-20 16:34:55 +0400

    72. Edit Distance

---
+++ src/main/java/leetCodeSolutions/mediumProblems/editDistance72/Main.java
@@ -0,0 +1,9 @@
+package leetCodeSolutions.mediumProblems.editDistance72;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int result = solution.minDistance("horse","ros");
+        System.out.println(result);
+    }
+}
