7a5befe9b2cd2a47ca467f7360908b850fb5c6fb
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-21 17:12:57 +0400

    309. Best Time to Buy and Sell Stock with Cooldown

--- src/main/java/leetCodeSolutions/mediumProblems/bestTimeToBuyAndSellStockWithCooldown309/Solution.java
+++ src/main/java/leetCodeSolutions/mediumProblems/bestTimeToBuyAndSellStockWithCooldown309/Solution.java
@@ -1,7 +1,18 @@
 package leetCodeSolutions.mediumProblems.bestTimeToBuyAndSellStockWithCooldown309;
 
-public class Solution {
+class Solution {
     public int maxProfit(int[] prices) {
+        int sell = 0;
+        int prevSell = 0;
+        int buy = Integer.MIN_VALUE;
+        int prevBuy = 0;
 
+        for(int price:prices){
+            prevBuy = buy;
+            buy = Math.max(prevSell-price,prevBuy);
+            prevSell = sell;
+            sell = Math.max(prevBuy+price,prevSell);
+        }
+        return sell;
     }
 }
