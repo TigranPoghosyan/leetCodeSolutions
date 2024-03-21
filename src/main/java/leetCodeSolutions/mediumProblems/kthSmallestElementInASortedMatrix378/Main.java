38ef2701781d950986704b94f7055fa3e5b7daca
Tigran Poghosyan <yerevan7971@gmail.com>
2024-03-21 17:49:26 +0400

    378. Kth Smallest Element in a Sorted Matrix

--- src/main/java/leetCodeSolutions/mediumProblems/kthSmallestElementInASortedMatrix378/Main.java
+++ src/main/java/leetCodeSolutions/mediumProblems/kthSmallestElementInASortedMatrix378/Main.java
@@ -2,6 +2,9 @@ package leetCodeSolutions.mediumProblems.kthSmallestElementInASortedMatrix378;
 
 public class Main {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        int[][] matrix = {{1, 5, 9}, {10, 11, 13}, {12, 13, 15}};
+        int result = solution.kthSmallest(matrix, 8);
+        System.out.println(result);
     }
 }
--- src/main/java/leetCodeSolutions/mediumProblems/kthSmallestElementInASortedMatrix378/Solution.java
+++ src/main/java/leetCodeSolutions/mediumProblems/kthSmallestElementInASortedMatrix378/Solution.java
@@ -1,5 +1,30 @@
 package leetCodeSolutions.mediumProblems.kthSmallestElementInASortedMatrix378;
 
-public class Solution {
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int rows = matrix.length;
+        int columns = matrix[0].length;
 
+        int low = matrix[0][0];
+        int high = matrix[rows-1][columns-1];
+
+        while(low < high){
+            int mid = low + (high - low) / 2;
+            int count = 0;
+            int j = columns-1;
+
+            for (int[] numbers : matrix) {
+                while (j >= 0 && numbers[j] > mid) {
+                    j--;
+                }
+                count += j + 1;
+            }
+            if(count < k){
+                low = mid + 1;
+            }else{
+                high = mid;
+            }
+        }
+        return low;
+    }
 }
